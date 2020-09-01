bef65529da81546a7bfafbb49c96e0979fe54955
Ab Qyum <qyumjahid@gmail.com>
2020-09-01 11:36:01 +0600

    Create weights.py

---
+++ weights.py
@@ -0,0 +1,7 @@
+def save_weights(epoch, model):
+    if not os.path.exists(MODEL_DIR):
+        os.makedirs(MODEL_DIR)
+    model.save_weights(os.path.join(MODEL_DIR, 'weights.{}.h5'.format(epoch)))
+
+def load_weights(epoch, model):
+    model.load_weights(os.path.join(MODEL_DIR, 'weights.{}.h5'.format(epoch)))
